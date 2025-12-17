d1229b5508263f283cbc42a6f1b74b76bece23fa
0xEthan <ethanzhang@creditease.cn>
2025-12-17 18:02:22 +0800

    feat: add sign typed data support for tron

--- src/components/ActionButtonListTron.tsx
+++ src/components/ActionButtonListTron.tsx
@@ -6,6 +6,11 @@ import {
 } from '../config/constant'
 import { useWallet } from '@tronweb3/tronwallet-adapter-react-hooks'
 import { useWalletModal } from '@tronweb3/tronwallet-adapter-react-ui'
+import { NetworkType } from '@tronweb3/tronwallet-abstract-adapter'
+import { chainIdNetworkMap } from '@tronweb3/tronwallet-adapters'
+
+const getChainIdByNetwork = (network: NetworkType) =>
+  Object.entries(chainIdNetworkMap).find(([, v]) => v === network)?.[0]
 
 // test transaction
 const TEST_TX = {
@@ -89,6 +94,65 @@ export const ActionButtonList = ({
     sendSignMsg(sig)
   }
 
+  const handleSignTypedData = async () => {
+    const tron = window.tron
+    if (!tron) throw Error('Tron is not installed')
+    const tronWeb_ = tron.tronWeb
+
+    if (!tronWeb_) throw Error('TronWeb is not available')
+
+    // All properties on a domain are optional
+    const domain = {
+      name: 'TRON Mail',
+      // version: '1',
+      chainId: getChainIdByNetwork(NetworkType.Nile),
+      verifyingContract: 'TUe6BwpA7sVTDKaJQoia7FWZpC9sK8WM2t',
+    }
+
+    // The named list of all type definitions
+    const types = {
+      Person: [
+        { name: 'name', type: 'string' },
+        { name: 'wallet', type: 'address' },
+      ],
+      Mail: [
+        { name: 'from', type: 'Person' },
+        { name: 'to', type: 'Person' },
+        { name: 'contents', type: 'string' },
+      ],
+    }
+
+    // The data to sign
+    const value = {
+      from: {
+        name: 'Cow',
+        wallet: 'TUg28KYvCXWW81EqMUeZvCZmZw2BChk1HQ',
+      },
+      to: {
+        name: 'Bob',
+        wallet: 'TT5rFsXYCrnzdE2q1WdR9F2SuVY59A4hoM',
+      },
+      contents: 'Hello, Bob!',
+    }
+
+    const tronlinkAddress = tronWeb_.defaultAddress.base58
+    console.log('tronlinkAddress: ', tronlinkAddress)
+
+    const signature = await tronWeb_.trx._signTypedData(domain, types, value)
+
+    const result = await tronWeb_.trx.verifyTypedData(
+      domain,
+      types,
+      value,
+      signature,
+      tronlinkAddress,
+    )
+
+    console.log('verify result: ', result)
+
+    sendSignMsg(signature)
+  }
+
   // function to get the balance
   const handleGetBalance = async () => {
     if (!wallet || !walletAddress) throw Error('user is disconnected')
@@ -105,6 +169,7 @@ export const ActionButtonList = ({
           <button onClick={handleDisconnect}>Disconnect</button>
           {/* <button onClick={() => switchNetwork(networks[1])}>Switch</button> */}
           <button onClick={handleSignMsg}>Sign msg</button>
+          <button onClick={handleSignTypedData}>Sign TypedData</button>
           <button onClick={handleSendTx}>Send tx</button>
           <button onClick={handleGetBalance}>Get Balance</button>
         </div>
